b323ab5399a46dae580ef0b87bf793d6287a57b7
Stephen Cocuzza <scocuzza3@gmail.com>
2020-12-07 16:56:23 -0500

    Part One

---
+++ src/SongList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, Button} from 'semantic-ui-react';
+function SongList(props){
+  const songs = props.songs.map((song) => {
+    return (
+        <Card key={song.id}>
+          <Card.Content>
+            <Card.Header>{song.title}</Card.Header>
+            <Card.Description>{song.artist}</Card.Description>
+            <Card.Description>{song.album}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Button>DeleteSong</Button>
+            <Button>Edit Song</Button>
+          </Card.Content>
+        </Card>
+        )
+  })
+  return (
+      <Card.Group>
+        { songs }
+      </Card.Group>
+    )
+}
+export default SongList
